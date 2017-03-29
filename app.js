663d2e65207b601e8478be5da270de29eebe6f0f
ljyockey <linyockey@gmail.com>
2017-03-29 07:19:03 -0700

    added functions

--- app.js
+++ app.js
@@ -1,12 +1,39 @@
+var youTubeUrl = 'https://www.googleapis.com/youtube/v3/search';
+
+
+function getApiData(searchItem, callback) {
+	var query = {
+		part: 'snippet',
+		key: 'AIzaSyC65vVs3VxOP4s8DH92WGLYJd8zTc5qqlU',
+		q: searchItem,
+	}
+	$.getJSON(youTubeUrl, query, callback);
+}
+
+function displayYouTubeResults(data) {
+	var results = '';
+	console.log(data.items);
+	if (data.items) {
+		data.items.forEach(function(item) {
+			results += '<img src="' + item.snippet.thumbnail.medium.url + '">"';
+		})
+	}
+	else {
+		results += '<p>No results</p>';
+	}
+	$('.results').html(results);
+}
 
 function submitListener() {
 	$('.search-form').submit(function(e) {
 		e.preventDefault();
-		var input = $(this).find('.query').val();
+		var query = $(this).find('.query').val();
+		getApiData(query, displayYouTubeResults);
 	})
 }
 
 $(function() {
+	submitListener();
 
 })
 /*  I need to get the value of the user's input;
