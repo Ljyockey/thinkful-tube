0415dca1a0cc9d557686533497a20fc6e77ac3f6
ljyockey <linyockey@gmail.com>
2017-03-29 21:04:51 -0700

    got next and previous page buttons working

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
 var youTubeUrl = 'https://www.googleapis.com/youtube/v3/search';
+var prevPageButton = ('<button class="prevPage" type="submit">' +
+		'Previous</button>');
 var nextPageButton = ('<button class="nextPage" type="submit">' +
 		'Next</button>');
 
-function getApiData(searchItem, callback) {
+
+//retrieves data from YouTube
+function getApiData(searchItem, callback, token) {
 	var query = {
 		part: 'snippet',
 		key: 'AIzaSyC65vVs3VxOP4s8DH92WGLYJd8zTc5qqlU',
 		q: searchItem,
+		pageToken: token,
 		type: 'video'
 	}
 	$.getJSON(youTubeUrl, query, callback);
 }
 
+//displays YouTube data to DOM
 function displayYouTubeResults(data) {
 	var results = '';
+	var pageQuery = $('.search-form').find('.query').val();
+
+	//checks if sure returned any results
 	if (data.items.length !== 0) {
 		console.log(data);
 		data.items.forEach(function(item) {
@@ -22,14 +31,33 @@ function displayYouTubeResults(data) {
 			item.id.videoId + '" target="blank">' +
 			'<img src="' + item.snippet.thumbnails.medium.url + '"></a></p>';
 		})
-		if (data.pageInfo.resultsPerPage < data.pageInfo.totalResults) {
+		//checks if there needs to be a previous page button and calls its function
+		if (data.prevPageToken) {
+			results += prevPageButton;
+			prevPageListener(data.prevPageButton, pageQuery)
+			}
+		//checks if there needs to be a next page button and calls its function	
+		if (data.nextPageToken) {
 			results += nextPageButton;
+			nextPageListener(data.nextPageToken, pageQuery);
 			}
 		}
 	else {
 		results += '<p>No results</p>';
 		}
 	$('.results').html(results);
+}
+
+function prevPageListener(token, query) {
+	$('.results').on('click', '.prevPage', function(e){
+		getApiData(query, displayYouTubeResults, token);
+	})	
+}
+
+function nextPageListener(token, query) {
+	$('.results').on('click', '.nextPage', function(e){
+		getApiData(query, displayYouTubeResults, token);
+	})
 }
 
 function submitListener() {
@@ -44,6 +72,3 @@ $(function() {
 	submitListener();
 
 })
-/*  I need to get the value of the user's input;
-make a function that creates JSON for user's input
-display the thumbnail of that JSON */
